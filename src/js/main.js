7f40534e7b389b4e122733b9be2d92a4edd22ec5
jedai037 <60815507+jedai037@users.noreply.github.com>
2020-03-23 00:46:09 +0200

    джава добавлена

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
 const button = document.querySelector('#button');
 const modal = document.querySelector('#modal');
-const close = document.querySelector('#close');
+const exit = document.querySelector('#close');
 
 button.addEventListener('click', function() {
     modal.classList.add('modal_active');
     setTimeout(modalClose, 5000);
 });
 
-close.addEventListener('click', function() {
+exit.addEventListener('click', function() {
     modalClose();
 });
 
 let modalClose = () => {
     modal.classList.remove('modal_active');
-}
+};
